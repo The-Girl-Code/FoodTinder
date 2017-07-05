6f37e23fe4b90d804df45990158c11cba8d558df
Aditi Gnanasekar <aditi.gnanam@gmail.com>
2017-07-05 14:40:02 -0700

    added buttons on Main.storyboard
    yay

---
+++ FoodTinder/FavoritesViewController.swift
@@ -0,0 +1,9 @@
+//
+//  FavoritesViewController.swift
+//  FoodTinder
+//
+//  Created by Aditi Gnanasekar on 7/5/17.
+//  Copyright © 2017 The Girl Code. All rights reserved.
+//
+
+import Foundation
